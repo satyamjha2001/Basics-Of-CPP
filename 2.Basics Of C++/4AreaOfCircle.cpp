6401ab5b91f3e6092068df2fe73b4988b3d05754
Satyam Kumar Jha <109470357+satyamjha2001@users.noreply.github.com>
2023-08-21 00:03:05 +0530

    commit

---
+++ 2.Basics Of C++/4AreaOfCircle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+float r;
+cout<<"Enter radius of circle: ";
+cin>>r;
+cout<<"Area of circle is: "<<(r*r*3.14);
+return 0;
+}
---
+++ 2.Basics Of C++/5VolumeOfCuboid.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+float l,b,h;
+cout<<"Enter length, breadth, and height of cuboid: ";
+cin>>l>>b>>h;
+cout<<"Volume of cuboid is: "<<(l*b*h);
+return 0;
+}
